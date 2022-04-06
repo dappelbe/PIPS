1cf8a28057a03bb40e889646266a5cd086c258b4
dappelbe <duncan.appelbe@ndorms.ox.ac.uk>
2022-04-06 16:36:48 +0100

    Added first cypress test

---
+++ code/www/PIPSIntervention/tests/Frontend/cypress/integration/001 - Unauthenticated Pages/001.001 Root redirects To Login when NOT Authenticated.js
@@ -0,0 +1,11 @@
+//-- ================================================================================= --//
+//-- Summary :: Unauthenticated Cypress tests
+//-- Created :: 05Apr2022
+//-- Author  :: Duncan Appelbe (duncan.appelbe@ndorms.ox.ac.uk)
+//-- ================================================================================= --//
+describe('001.001 - / redirects to /login when not authenticated', () => {
+    it('Accesses / and checks to see that we are redirected to the login page', () => {
+        cy.visit('http://localhost');
+        cy.url().should('eq', 'http://localhost/login');
+    });
+});
