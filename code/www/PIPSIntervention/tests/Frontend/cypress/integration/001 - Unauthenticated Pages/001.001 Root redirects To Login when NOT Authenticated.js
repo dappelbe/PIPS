8bc6cf2f401f5f842df1e7704a72e683751a4efd
dappelbe <duncan.appelbe@ndorms.ox.ac.uk>
2022-04-06 17:39:26 +0100

    note sure

--- code/www/PIPSIntervention/tests/Frontend/cypress/integration/001 - Unauthenticated Pages/001.001 Root redirects To Login when NOT Authenticated.js
+++ code/www/PIPSIntervention/tests/Frontend/cypress/integration/001 - Unauthenticated Pages/001.001 Root redirects To Login when NOT Authenticated.js
@@ -4,8 +4,10 @@
 //-- Author  :: Duncan Appelbe (duncan.appelbe@ndorms.ox.ac.uk)
 //-- ================================================================================= --//
 describe('001.001 - / redirects to /login when not authenticated', () => {
-    it('Accesses / and checks to see that we are redirected to the login page', () => {
-        cy.visit('http://localhost');
-        cy.url().should('eq', 'http://localhost/login');
+    context('Unauthenticated', () => {
+        it('Accesses / and checks to see that we are redirected to the login page', () => {
+            cy.visit('http://localhost');
+            cy.url().should('eq', 'http://localhost/login');
+        });
     });
 });
