f68c5193efc3be9ce28e4e2c9390731408fded17
dappelbe <duncan.appelbe@ndorms.ox.ac.uk>
2022-04-07 21:02:25 +0100

    making use of baseurl

--- code/www/PIPSIntervention/tests/Frontend/cypress/integration/001 - Unauthenticated Pages/001.001 Root redirects To Login when NOT Authenticated.js
+++ code/www/PIPSIntervention/tests/Frontend/cypress/integration/001 - Unauthenticated Pages/001.001 Root redirects To Login when NOT Authenticated.js
@@ -6,8 +6,8 @@
 describe('001.001 - / redirects to /login when not authenticated', () => {
     context('Unauthenticated', () => {
         it('Accesses / and checks to see that we are redirected to the login page', () => {
-            cy.visit('http://localhost');
-            cy.url().should('eq', 'http://localhost/login');
+            cy.visit('/');
+            cy.url().should('eq', Cypress.config().baseUrl + '/login');
         });
     });
 });
