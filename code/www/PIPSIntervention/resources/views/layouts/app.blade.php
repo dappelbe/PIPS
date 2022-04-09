a3cdd30d7793a3ec525e85085e35657caf67d48a
dappelbe <duncan.appelbe@ndorms.ox.ac.uk>
2022-04-09 13:21:49 +0100

    added translate logo to navbar with link to study site

--- code/www/PIPSIntervention/resources/views/layouts/app.blade.php
+++ code/www/PIPSIntervention/resources/views/layouts/app.blade.php
@@ -26,6 +26,9 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <img src="{{asset('images/pips-logo.png')}}" alt="PIPS Logo" class="img-thumbnail" style="height: 2em;"/>
                 </a>
+                <a class="navbar-brand" href="https://translate.octru.ox.ac.uk/" target="_blank">
+                    <img src="{{asset('images/translate-logo.png')}}" alt="TRANSLATE Logo" class="img-thumbnail" style="height: 2em;"/>
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
