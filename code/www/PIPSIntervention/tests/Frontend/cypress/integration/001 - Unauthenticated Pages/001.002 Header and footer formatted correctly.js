0a54e22b58d0759f147b5514d6c82ee43708e0d7
dappelbe <duncan.appelbe@ndorms.ox.ac.uk>
2022-04-10 21:34:27 +0100

    Added UI tests for cypress.

---
+++ code/www/PIPSIntervention/tests/Frontend/cypress/integration/001 - Unauthenticated Pages/001.002 Header and footer formatted correctly.js
@@ -0,0 +1,50 @@
+//-- ================================================================================= --//
+//-- Summary :: Check that the header and footer are formatted correctly
+//-- Created :: 10Apr2022
+//-- Author  :: Duncan Appelbe (duncan.appelbe@ndorms.ox.ac.uk)
+//-- ================================================================================= --//
+describe( '001.002 - Header and footer formatted correctly', () => {
+    context('Unauthenticated', () => {
+        it('Accesses the home page and checks that the header and footer are formatted as expected.', () =>{
+            cy.visit('/');
+            //-- =========================================== --//
+            //-- Header
+            //-- =========================================== --//
+            //-- PIPS Logo
+            cy.get('[data-cy=navlink-pips-home]')
+                .should('be.visible');
+            //-- Translate Logo
+            cy.get('[data-cy=navlink-translate-logo]')
+                .should('be.visible');
+            //-- =========================================== --//
+            //-- Footer
+            //-- =========================================== --//
+            cy.get('#footer')
+                .should('be.visible');
+            cy.get('#footer')
+                .contains('Contact')
+                .should('be.visible');
+            cy.get('[data-cy=mailto-vb]')
+                .should('be.visible');
+            cy.get('[data-cy=mailto-da]')
+                .should('be.visible');
+            cy.get('#footer')
+                .contains('Funded By')
+                .should('be.visible');
+            cy.get('[data-cy=logo-nihr]')
+                .should('be.visible');
+            cy.get('#footer')
+                .contains('Hosted By')
+                .should('be.visible');
+            cy.get('[data-cy=logo-octru]')
+                .should('be.visible');
+            cy.get('[data-cy=link_foi]')
+                .should('be.visible');
+            cy.get('[data-cy=link_privacy]')
+                .should('be.visible');
+            cy.get('[data-cy=link_accessibility]')
+                .should('be.visible');
+
+        })
+    });
+});
---
+++ code/www/PIPSIntervention/tests/Frontend/cypress/integration/001 - Unauthenticated Pages/001.003 Login Form is formatted correctly.js
@@ -0,0 +1,39 @@
+//-- ================================================================================= --//
+//-- Summary :: Check that the login page is formatted correctly
+//-- Created :: 10Apr2022
+//-- Author  :: Duncan Appelbe (duncan.appelbe@ndorms.ox.ac.uk)
+//-- ================================================================================= --//
+describe('001.003 - Login form is correctly formatted', () => {
+   context('Page formatted correctly', () => {
+       it('Opens the login page and checks that it is correctly setup', () => {
+          cy.visit('/');
+          //-- Nav bar
+          cy.get('[data-cy=link-login]').should('be.visible');
+          cy.get('[data-cy=link-register]').should('be.visible');
+          //-- Form
+          cy.get('[data-cy=login-logo-pips]').should('be.visible');
+          cy.get('[data-cy=login-logo-study]').should('be.visible');
+           cy.get('[data-cy=login-title]').should('be.visible');
+           cy.get('[data-cy=login-title]').should('contain.text', 'Welcome to the OCTRU Participant Information PortalS');
+           cy.get('[data-cy=login-instruction]').should('be.visible');
+           cy.get('[data-cy=login-instruction]').should('contain.text', 'Please enter your email address and PIPS password to access your Portal');
+           cy.get('[data-cy=login-input-email-label]').should('be.visible');
+           cy.get('[data-cy=login-input-email-label]').should('contain.text', 'Email Address');
+           cy.get('[data-cy=login-input-email]').should('be.visible');
+           cy.get('[data-cy=login-input-email]').invoke('attr', 'type').should('eq', 'email');
+           cy.get('[data-cy=login-input-password-label]').should('be.visible');
+           cy.get('[data-cy=login-input-password-label]').should('contain.text', 'Password');
+           cy.get('[data-cy=login-input-password]').should('be.visible');
+           cy.get('[data-cy=login-input-password]').invoke('attr', 'type').should('eq', 'password');
+           cy.get('[data-cy=login-input-remember_me-label]').should('be.visible');
+           cy.get('[data-cy=login-input-remember_me-label]').should('contain.text', 'Remember Me');
+           cy.get('[data-cy=login-input-remember_me]').should('be.visible');
+           cy.get('[data-cy=login-input-remember_me]').invoke('attr', 'type').should('eq', 'checkbox');
+           cy.get('[data-cy=login-submit]').should('be.visible');
+           cy.get('[data-cy=login-submit]').invoke('attr', 'type').should('eq', 'submit');
+           cy.get('[data-cy=login-submit]').should('contain.text', 'Login');
+           cy.get('[data-cy=login-forgot-password]').should('be.visible');
+           cy.get('[data-cy=login-forgot-password]').should('contain.text', 'Forgot Your Password?');
+       });
+   }) ;
+});
