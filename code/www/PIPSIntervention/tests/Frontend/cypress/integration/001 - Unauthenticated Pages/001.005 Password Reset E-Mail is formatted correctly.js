243ab3d4f4624807e6d4d77dbbb3ac76b7968d62
dappelbe <duncan.appelbe@ndorms.ox.ac.uk>
2022-04-13 09:00:16 +0100

    Investigating why mail tests are failing on GitHub, but pass on localhost - added an invoke to call replaceAll and have chained all should statements to make more efficient - hopefully!

--- code/www/PIPSIntervention/tests/Frontend/cypress/integration/001 - Unauthenticated Pages/001.005 Password Reset E-Mail is formatted correctly.js
+++ code/www/PIPSIntervention/tests/Frontend/cypress/integration/001 - Unauthenticated Pages/001.005 Password Reset E-Mail is formatted correctly.js
@@ -27,7 +27,17 @@ describe('001.005 Password Reset E-Mail is formatted correctly', () => {
             cy.mhGetAllMails()
                 .mhFirst()
                 .mhGetBody()
-                .should('contain', 'Hello!');
+                .invoke('replaceAll', '=\r\n', '')
+                .invoke('replaceAll', '\r\n', '')
+                .should('contain', 'Hello!')
+                .should('contain', 'You are receiving this email because we received a password reset request for your account.')
+                .should('contain', 'This password reset link will expire in 60 minutes.')
+                .should('contain', 'If you did not request a password reset, no further action is required.')
+                .should('contain', 'Regards,')
+                .should('contain', 'PIPs')
+                .should('contain', "If you're having trouble clicking the")
+                .should('contain', "button, copy and paste the URL below")
+                .should('contain', 'into your web browser:');
         });
     });
 });
