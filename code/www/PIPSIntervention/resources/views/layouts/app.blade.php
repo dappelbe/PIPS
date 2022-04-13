c36d3e4fb3bfcfc2ed9c362e64f2bb30792a1abd
dappelbe <duncan.appelbe@ndorms.ox.ac.uk>
2022-04-13 13:51:45 +0100

    removed register link

--- code/www/PIPSIntervention/resources/views/layouts/app.blade.php
+++ code/www/PIPSIntervention/resources/views/layouts/app.blade.php
@@ -48,13 +48,7 @@
                                     <a class="nav-link" href="{{ route('login') }}" data-cy="link-login">{{ __('Login') }}</a>
                                 </li>
                             @endif
-<!--
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}" data-cy="link-register">{{ __('Register') }}</a>
-                                </li>
-                            @endif
--->
+
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown"
