865e315564d46c19bbb0be0112507b021c706ace
dappelbe <duncan.appelbe@ndorms.ox.ac.uk>
2022-04-20 15:47:24 +0100

    Added code to extract a list from the db

--- code/www/PIPSIntervention/app/Models/ConsentForm.php
+++ code/www/PIPSIntervention/app/Models/ConsentForm.php
@@ -4,11 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class ConsentForm extends Model
 {
     use HasFactory;
     protected $table = 'consentform';
     protected $fillable = array('pis', 'voluntary', 'data', 'agree', 'name', 'consentdate', 'ppt_signature');
+
+    /***
+     * Get a list of all data in the table
+     * @return mixed[]
+     */
+   public static function List() {
+       return DB::table('consentform')->get()->toArray();
+   }
 
 }
