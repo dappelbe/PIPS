81d5b1ff2cc475d1314e9b4c07a2f32082321433
dappelbe <duncan.appelbe@ndorms.ox.ac.uk>
2022-06-17 14:18:09 +0100

    Updated to test that home page contains the correct information

--- code/www/PIPSIntervention/tests/Frontend/cypress/e2e/002 - Site User (Authenticated)/002.001 User Login - View Pages.cy.js
+++ code/www/PIPSIntervention/tests/Frontend/cypress/e2e/002 - Site User (Authenticated)/002.001 User Login - View Pages.cy.js
@@ -18,6 +18,47 @@ describe( '002.001 - User logs in, view pages', () => {
             cy.get('[data-cy=login-submit]').click();
             //-- Assert
             cy.url().should('eq', Cypress.config().baseUrl + '/home');
+            //-- hdr & footer
+            cy.get('[data-cy=navlink-pips-home]').should('be.visible');
+            cy.get('#footer').should('be.visible');
+            cy.get('#footer').contains('Contact').should('be.visible');
+            cy.get('[data-cy=mailto-vb]').should('be.visible');
+            cy.get('[data-cy=mailto-da]').should('be.visible');
+            cy.get('#footer').contains('Funded By').should('be.visible');
+            cy.get('[data-cy=logo-nihr]').should('be.visible');
+            cy.get('#footer').contains('Hosted By').should('be.visible');
+            cy.get('[data-cy=logo-octru]').should('be.visible');
+            cy.get('[data-cy=link_foi]').should('be.visible');
+            cy.get('[data-cy=link_privacy]').should('be.visible');
+            cy.get('[data-cy=link_accessibility]').should('be.visible');
+            //-- Welcome popup
+            cy.get('[data-cy=welcome-alert]').should('be.visible');
+            cy.get('[data-cy=welcome-hdr]').should('be.visible');
+            cy.get('[data-cy=welcome-hdr]').should('contain.text','WELCOME');
+            cy.get('[data-cy=w-p1]').should('contain.text','Hello PIPS thank you for agreeing to log into and hopefully find this mini website (portal) useful.');
+            cy.get('[data-cy=w-p2]').should('contain.text','The last time that you logged in was: ');
+            cy.get('[data-cy=w-p3]').should('contain.text','If you wish to send a message to the Central CRAFFT study team – please click');
+            cy.get('[data-cy=w-p4]').should('contain.text','If you wish to send a message or give any feedback to the PIPS team');
+            cy.get('[data-cy=w-p5]').should('contain.text','Click on the X in the top right-hand corner to clear this message');
+            //- Card #1
+            cy.get('[data-cy=c1]').should('be.visible');
+            cy.get('[data-cy=c1-hdr]').should('contain.text','This is the personalised portal for PIPS in the CRAFFT study');
+            cy.get('[data-cy=c1-b1]').should('contain.text','CR-RAC-10035');
+            cy.get('[data-cy=c1-b1-h3]').should('contain.text','Your CRAFFT trial number');
+            cy.get('[data-cy=c1-b2-hdr]').should('contain.text','You were recruited at the Royal Aberdeen Children\'s Hospital RAC.');
+            cy.get('[data-cy=c1-b3-hdr]').should('contain.text','You were allocated to the Non-surgical casting arm.');
+            cy.get('[data-cy=c1-b4-hdr]').should('contain.text','You are the 1st participant who has agreed to take part in the CRAFFT trial.');
+            //-- Card 2
+            cy.get('[data-cy=c2]').should('be.visible');
+            cy.get('[data-cy=c2-btn1]').should('contain.text','Where am I in my study journey?');
+            cy.get('[data-cy=c2-btn2]').should('contain.text','The progress of the CRAFFT study');
+            cy.get('[data-cy=c2-btn3]').should('contain.text','What is due for me next?');
+            cy.get('[data-cy=c2-btn4]').should('contain.text','How do I contact the CRAFFT study team?');
+            //-- card 3
+            cy.get('[data-cy=c3]').should('be.visible');
+            cy.get('[data-cy=c3-hdr]').should('contain.text','Download');
+            cy.get('[data-cy=c3-pips-hdr]').should('contain.text','PIPS');
+            cy.get('[data-cy=c3-study-hdr]').should('contain.text','CRAFFT');
         });
     });
 });
