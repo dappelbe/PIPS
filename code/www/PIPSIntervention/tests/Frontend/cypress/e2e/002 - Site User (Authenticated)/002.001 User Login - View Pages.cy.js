2f97a05be73b6b3dccaab85656b316f8447d90a8
dappelbe <duncan.appelbe@ndorms.ox.ac.uk>
2022-09-22 13:10:41 +0100

    002.001 User Login
    Updated to make use of functions so minimising code smell

--- code/www/PIPSIntervention/tests/Frontend/cypress/e2e/002 - Site User (Authenticated)/002.001 User Login - View Pages.cy.js
+++ code/www/PIPSIntervention/tests/Frontend/cypress/e2e/002 - Site User (Authenticated)/002.001 User Login - View Pages.cy.js
@@ -6,56 +6,13 @@
 describe( '002.001 - User logs in, view pages', () => {
     context('Authenticated - User', () => {
         it('Setup database', function () {
-            cy.task('queryDb', `INSERT INTO studydetails (id, created_at, updated_at, studyname,
-                                                          apiurl, apikey, studylogo, studyemail,
-                                                          studyphone, studyaddress, studyaccruallink,
-                                                          uploadedpis, studyrandomisationreportid,
-                                                          randonumfield, allocationfield, sitenamefield,
-                                                          studystatusreportid, expectedrecruits,
-                                                          randodatefield) VALUES (1, '2022-04-22 10:42:53',
-                                                                                  '2022-04-22 10:42:53', 'CRAFFT',
-                                                                                  'https://redcap-cctr.octru.ox.ac.uk/api/',
-                                                                                  'A71604FCD642E04E82712BAFBDFE09AF',
-                                                                                  'signature.png',
-                                                                                  'crafft@ndorms.ox.ac.uk',
-                                                                                  '01865 228929',
-                                                                                  'Oxford Trauma\\r\\nKadoorie Centre\\r\\nNDORMS\\r\\nUniversity of Oxford\\r\\nJohn Radcliffe Hospital\\r\\nHeadley Way\\r\\nOxford OX3 9DU',
-                                                                                  'https://kadoorie.octru.ox.ac.uk/CRAFFT_SIMS/Recruitment',
-                                                                                  1, 579, 'ra_subj_id',
-                                                                                  'ra_treat_alloc', 'ra_cte_id', 784,
-                                                                                  784, 'ra_date');`).then((result) => {
-                expect(result.affectedRows).to.equal(1)
-            });
-
-            cy.task('queryDb', `INSERT INTO users (id, name, email, email_verified_at,
-                                                   password, remember_token, created_at, updated_at,
-                                                   last_login_at, last_login_ip, randomisation_number,
-                                                   studyid) VALUES (2, 'Test User',
-                                                                                  'test.user@Noidea.com', NULL,
-                                                                                  '$2y$10$8T9RWIS3n3WQPhKArjL/H.HhDs.PgNfJ8/usl/l/6ktInJvksbe62',
-                                                                                  'Vp5Jh0f1beK5qDBnWCwpklr2mUmt6LwL2uqGhAKF4iVIe3JACKQRLSet5vLZ',
-                                                                                  '2022-04-22 12:42:37',
-                                                                                  '2022-09-20 16:15:20',
-                                                                                  '2022-09-20 17:15:20',
-                                                                                  '195.213.65.98',
-                                                                                  'CR-RAC-10035',
-                                                                                  1);`).then((result) => {
-                expect(result.affectedRows).to.equal(1)
-            });
-
-
+            cy.pipsDBAddStudy();
+            cy.pipsDBAddUser();
         });
         it('Login as test.user@Noidea.com, confirm that the home page is displayed', () => {
             //-- Arrange
-            cy.visit('/login');
-            cy.get('[data-cy=login-input-email]')
-                .clear()
-                .type('test.user@Noidea.com');
-            cy.get('[data-cy=login-input-password]')
-                .clear()
-                .type('MyPassword4PIPs');
+            cy.pipsLogin('user');
             //-- Act
-            cy.get('[data-cy=login-submit]').click();
             //-- Assert
             cy.url().should('eq', Cypress.config().baseUrl + '/home');
             //-- hdr & footer
@@ -102,15 +59,7 @@ describe( '002.001 - User logs in, view pages', () => {
         });
         it('Login as test.user@Noidea.com, access the contact details page', () => {
             //-- Arrange
-            cy.visit('/login');
-            cy.get('[data-cy=login-input-email]')
-                .clear()
-                .type('test.user@Noidea.com');
-            cy.get('[data-cy=login-input-password]')
-                .clear()
-                .type('MyPassword4PIPs');
-            //-- Act
-            cy.get('[data-cy=login-submit]').click();
+            cy.pipsLogin('user');
             cy.get('[data-cy=contact]').click();
             //-- Assert
             cy.url().should('eq', Cypress.config().baseUrl + '/contact');
@@ -141,13 +90,12 @@ describe( '002.001 - User logs in, view pages', () => {
             cy.get('[data-cy=back-button]').should('be.visible');
             cy.get('[data-cy=back-button]').should('contain.text','Back');
         });
+        it( 'Login as test.user@Noidea.com, access the where am I in my study journey', () => {
+
+        });
         it('Tidy database', function() {
-            cy.task('queryDb', 'DELETE FROM studydetails where id = 1;').then((result) => {
-                expect(result.affectedRows).to.equal(1)
-            });
-            cy.task('queryDb', 'DELETE FROM users where id = 2;').then((result) => {
-                expect(result.affectedRows).to.equal(1)
-            });
+            cy.pipsDBClearUser();
+            cy.pipsDBClearStudy();
         });
     });
 });
