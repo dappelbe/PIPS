5fa7dc622e93c434151473d326a7f0eead36a147
dappelbe <duncan.appelbe@ndorms.ox.ac.uk>
2022-11-25 10:44:33 +0000

    where.blade
    Added data-cy tags for cypress testing

--- code/www/PIPSIntervention/resources/views/home/where.blade.php
+++ code/www/PIPSIntervention/resources/views/home/where.blade.php
@@ -6,27 +6,27 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-header">
-                    <h3>This is the personalised portal for {{ Auth::user()->name }} in the {{$studyName}} study</h3>
+                    <h3 data-cy="main_hdr">This is the personalised portal for {{ Auth::user()->name }} in the {{$studyName}} study</h3>
                 </div>
                 <div class="card-body">
                     <div class="row">
                         <div class="alert alert-success">
-                            <h4>Where am I in my study journey?</h4>
+                            <h4 data-cy="where-am-i-hdr">Where am I in my study journey?</h4>
                         </div>
                     </div>
                     <div class="row">
                         <div class="alert alert-secondary">
                             <div class="row">
                                 <div class="col-12">
-                                    <strong>Table Key</strong>
+                                    <strong data-cy="table-key">Table Key</strong>
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col-3">
-                                    <span class="badge badge-success" style="background-color: green;">Form name</span> : Form completed
+                                <div class="col-3" data-cy="success-badge">
+                                    <span data-cy="success-element" class="badge badge-success" style="background-color: green;">Form name</span> : Form completed
                                 </div>
-                                <div class="col-3">
-                                    <span class="badge badge-danger" style="background-color: red;">Form name</span> : Form NOT completed
+                                <div class="col-3" data-cy="error-badge">
+                                    <span data-cy="error-element" class="badge badge-danger" style="background-color: red;">Form name</span> : Form NOT completed
                                 </div>
                             </div>
                         </div>
@@ -34,7 +34,7 @@
                     <div class="row flex-grow-1">
                         <div class="col-12">
                             <table id="logData" class="table table-striped w-auto table-responsive">
-                                <thead>
+                                <thead data-cy="logData-table">
                                     <tr>
                                         <th>Visit</th>
                                         <th>Forms</th>
@@ -55,7 +55,7 @@
                     </div>
                     <div class="row">
                         <div class="col-3">
-                            <a href="{{route('home')}}">
+                            <a href="{{route('home')}}" data-cy="home-btn">
                                 <button class="btn button-primary">
                                     <em class="fa-solid fa-arrow-left"></em> Back
                                 </button>
